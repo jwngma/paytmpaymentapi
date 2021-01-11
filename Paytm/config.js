a236f634514515e1430bb72364d9567b5c29ee63
jwngma basumatary <smkbty@gmail.com>
2021-01-11 08:43:36 +0530

    first

---
+++ Paytm/config.js
@@ -0,0 +1,13 @@
+// var PaytmConfig = {
+//   mid: "Kkatvd34334425459416",
+//   key: "fgkmv_RHsi6R@QXm",
+//   website: "WEBSTAGING",
+// };
+// module.exports.PaytmConfig = PaytmConfig;
+
+var PaytmConfig = {
+  mid: "NPtiMi63273535189560",
+  key: "MFS@%5t2lZ9LunC#",
+  website: "WEBSTAGING",
+};
+module.exports.PaytmConfig = PaytmConfig;
